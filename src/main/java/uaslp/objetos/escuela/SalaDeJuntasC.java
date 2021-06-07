14ad05531778d41533a754e4f6a2cb1b339606ae
Jesus <jesusgerardo.1315@hotmail.com>
2021-06-07 12:30:38 -0500

    Ejercicio 4 completado

---
+++ src/main/java/uaslp/objetos/escuela/SalaDeJuntasC.java
@@ -0,0 +1,19 @@
+package uaslp.objetos.escuela;
+
+public class SalaDeJuntasC implements SalaDeJuntas{
+
+    private static SalaDeJuntasC uniqueInstance;
+
+    private SalaDeJuntasC(){}
+
+    public static SalaDeJuntasC getInstance(){
+        if(uniqueInstance == null)
+            uniqueInstance = new SalaDeJuntasC();
+        return uniqueInstance;
+    }
+
+    @Override
+    public String getNombre() {
+        return "Sala C";
+    }
+}
