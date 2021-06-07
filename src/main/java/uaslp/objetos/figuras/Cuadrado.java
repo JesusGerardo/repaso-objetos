6f04038625a7669fd97df8079f50f5fa0377ac82
Jesus <jesusgerardo.1315@hotmail.com>
2021-06-06 21:15:13 -0500

    ejercicio 1 resuelto

--- src/main/java/uaslp/objetos/figuras/Cuadrado.java
+++ src/main/java/uaslp/objetos/figuras/Cuadrado.java
@@ -1,2 +1,23 @@
-package uaslp.objetos.figuras;public class Cuadrado {
+package uaslp.objetos.figuras;
+
+public class Cuadrado{
+    private double lado;
+    public Cuadrado(){
+    }
+
+    public Cuadrado(double lado) {
+        this.lado = lado;
+    }
+
+    public void setLado(double lado) {
+        this.lado = lado;
+    }
+
+    public double getLado() {
+        return lado;
+    }
+
+    public double getArea() {
+        return lado * lado;
+    }
 }
--- src/main/java/uaslp/objetos/figuras/PoligonoRegular.java
+++ src/main/java/uaslp/objetos/figuras/PoligonoRegular.java
@@ -1,2 +1,29 @@
-package uaslp.objetos.figuras;public class PoligonoRegular {
+package uaslp.objetos.figuras;
+import java.lang.Math;
+public class PoligonoRegular {
+    private double lado;
+    private int numeroDeLados;
+
+    public PoligonoRegular(int numeroDeLados) {
+        this.numeroDeLados = numeroDeLados;
+    }
+
+    public PoligonoRegular(int numeroDeLados, double lado) {
+        this.numeroDeLados = numeroDeLados;
+        this.lado = lado;
+    }
+
+    public void setLado(double lado) {
+        this.lado = lado;
+    }
+
+    public double getArea(){
+        double angulo = 360/numeroDeLados;
+        double apotema = (lado/2)/Math.tan(Math.toRadians(angulo/2));
+        return (lado*numeroDeLados*apotema)/2;
+    }
+
+    public double getLado() {
+        return lado;
+    }
 }
