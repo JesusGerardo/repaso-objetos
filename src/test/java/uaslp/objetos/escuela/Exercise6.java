51aa597194e0693b07ef3871c9bae7262c890976
Jesus <jesusgerardo.1315@hotmail.com>
2021-06-08 15:59:47 -0500

    Ejercicio 6 completado

--- src/test/java/uaslp/objetos/escuela/Exercise6.java
+++ src/test/java/uaslp/objetos/escuela/Exercise6.java
@@ -1,8 +1,10 @@
 package uaslp.objetos.escuela;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.*;
 
 public class Exercise6 {
 
@@ -18,6 +20,61 @@ public class Exercise6 {
     modificar el test existente "validarInterfacesCreadas"
 
      */
+    @Test
+    public void algoritmoACubrirAEqualsB(){
+        //Given
+        int a = 5;
+        int b = 5;
+        String c = "";
+        Dependencia1 dependencia1 = Mockito.mock(Dependencia1.class);
+        Dependencia2 dependencia2 = Mockito.mock(Dependencia2.class);
+        Dependencia3 dependencia3 = Mockito.mock(Dependencia3.class);
+        AlgoritmoX algoritmoX = new AlgoritmoX(dependencia1,dependencia2,dependencia3);
+        //When
+        algoritmoX.algoritmoACubrir(a,b,c);
+        //Then
+        Mockito.verify(dependencia1).save(c);
+        Mockito.verify(dependencia3).recover();
+        Mockito.verifyNoMoreInteractions(dependencia1,dependencia2,dependencia3);
+    }
+
+    @Test
+    public void AlgoritmoACubrirBIsGreaterThanA(){
+        //Given
+        int a = 5;
+        int b = 10;
+        String c = "";
+
+        Dependencia1 dependencia1 = Mockito.mock(Dependencia1.class);
+        Dependencia2 dependencia2 = Mockito.mock(Dependencia2.class);
+        Dependencia3 dependencia3 = Mockito.mock(Dependencia3.class);
+        AlgoritmoX algoritmoX = new AlgoritmoX(dependencia1,dependencia2,dependencia3);
+        //When
+        algoritmoX.algoritmoACubrir(a,b,c);
+        //Then
+        Mockito.verify(dependencia2).print(a, c);
+        Mockito.verify(dependencia3).recover();
+        Mockito.verifyNoMoreInteractions(dependencia1,dependencia2,dependencia3);
+    }
+
+    @Test
+    public void AlgoritmoACubrirAIsGreaterThanB(){
+        //Given
+        int a = 10;
+        int b = 5;
+        String c = "";
+
+        Dependencia1 dependencia1 = Mockito.mock(Dependencia1.class);
+        Dependencia2 dependencia2 = Mockito.mock(Dependencia2.class);
+        Dependencia3 dependencia3 = Mockito.mock(Dependencia3.class);
+        AlgoritmoX algoritmoX = new AlgoritmoX(dependencia1,dependencia2,dependencia3);
+        //When
+        algoritmoX.algoritmoACubrir(a,b,c);
+        //Then
+        Mockito.verify(dependencia3).send(a,b);
+        Mockito.verify(dependencia3).recover();
+        Mockito.verifyNoMoreInteractions(dependencia1,dependencia2,dependencia3);
+    }
 
     @Test
     public void validarInterfacesCreadas(){
